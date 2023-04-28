a437221149f86664ba41da531a925b36775f53ac
KinotiJames <mwirigikinoti7@gmail.com>
2023-04-27 17:25:47 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * function -  executed before main
+ * Return: no return.
+ */
+void function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
