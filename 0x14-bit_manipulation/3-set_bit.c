86b2067f2e86483513d8e264c5724c4a1fba1d47
KinotiJames <mwirigikinoti7@gmail.com>
2023-05-04 07:59:00 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,12 +9,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
-		 return (-1);
-	if (!((*n >> index) & 1))
-	{
-		*n += 1 << index;
-		return (1);
-	}
-	return (-1);
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
 }
